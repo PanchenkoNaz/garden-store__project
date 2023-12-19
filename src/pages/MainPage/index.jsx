d42fec6aa505b7f67e23a8dfb86b9c084de6fc6a
Nazar Panchenko <120579167+PanchenkoNaz@users.noreply.github.com>
2023-12-19 18:42:39 +0100

    fix main page

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react';
 import s from './style.module.css';
-import flowers from './garden store.jpg';
 import { Link } from 'react-router-dom';
 import CategoryItem from '../../components/CategoryItem';
 import { useSelector } from 'react-redux';
